3faba80ec00069d7280176217573d70bb166fca2
Camsca <camila_vass@hotmail.com>
2024-01-16 21:22:53 -0800

    exporting queries and mutations to client side with apollo/client

---
+++ client/src/utils/queries.js
@@ -0,0 +1,21 @@
+import { gql } from '@apollo/client';
+
+// Query to get the authenticated user's information
+export const GET_ME = gql`
+  query {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
